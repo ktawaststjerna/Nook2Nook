8366850f3f01de1c79694d7d99c08ce918250ec5
Kevin Tawaststjerna <ktawaststjerna@gmail.com>
2020-04-22 22:53:07 -0300

    destruction

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,7 @@ Rails.application.routes.draw do
 
   mount Sidekiq::Web => '/sidekiq'
 
-  resources :queue_list_to_users
-  resources :queue_lists
   resources :items
-  resources :listings
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
